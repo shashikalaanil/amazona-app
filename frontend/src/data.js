5c4815e10f4b7e00867e1d3bb8d427b4aa300d64
shashikalaanil <shashikala.ghaste@gmail.com>
2022-08-16 20:18:48 +0530

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Van Heusen',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:120,
+            brand:'Van Heusen',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Louis Phillippe',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:120,
+            brand:'Louis Phillippe',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Peter England',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:100,
+            brand:'Nike',
+            rating:4.8,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Oxemberg',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.0,
+            numReviews:10,
+            description:'High quality product'
+        },
+    ],
+}
+export default data;
